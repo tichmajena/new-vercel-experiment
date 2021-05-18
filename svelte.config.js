67d8ca65b544f43ed231984c0e14bda13a00b92c
tichmajena <tichmajena@gmail.com>
2021-05-18 14:41:43 +0200

    vercel-attempt-1

--- svelte.config.js
+++ svelte.config.js
@@ -1,15 +1,18 @@
+const node = require("@sveltejs/adapter-vercel");
+const pkg = require("./package.json");
+
 import preprocess from "svelte-preprocess";
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	preprocess: [
-		preprocess({
-			postcss: true
-		}),
-	],
-	kit: {
-		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte'
-	}
+  preprocess: [
+    preprocess({
+      postcss: true,
+    }),
+  ],
+  kit: {
+    // hydrate the <div id="svelte"> element in src/app.html
+    target: "#svelte",
+  },
 };
 
 export default config;
